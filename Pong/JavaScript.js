6e837b4c8afdf6e7b3d736ad9cc0f3add115b142
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-05-11 19:46:49 -0300

    racquet n colision

--- Pong/JavaScript.js
+++ Pong/JavaScript.js
@@ -1,10 +1,3 @@
-let xBolinha = 300;
-let yBolinha = 200;
-let diametro = 20;
-
-let velocidadeXBolinha = 4;
-let velocidadeYBolinha = 4;
-
 function setup() {
     createCanvas(500, 400);
 }
@@ -14,7 +7,20 @@ function draw() {
     mostraBolinha();
     movimentoBolinha();
     colisaoBolinha();
+    mostraRaquete();
+    movimentoRaquete();
+    colisaoRaquete();
 }
+
+//Bolinha
+
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 20;
+let raio = diametro / 2
+
+let velocidadeXBolinha = 4;
+let velocidadeYBolinha = 4;
 
 function mostraBolinha() {
     circle(xBolinha, yBolinha, diametro);
@@ -26,10 +32,36 @@ function movimentoBolinha() {
 }
 
 function colisaoBolinha() {
-    if (xBolinha > (width - 10) || xBolinha < 10) {
+    if (xBolinha + raio > width || xBolinha < raio) {
         velocidadeXBolinha *= -1
     }
-    if (yBolinha > (height - 10) || yBolinha < 10) {
+    if (yBolinha + raio > height || yBolinha < raio) {
         velocidadeYBolinha *= -1
+    }
+}
+
+//Raquete
+
+let xRaquete = 5;
+let yRaquete = 150;
+let larguraRaquete = 10;
+let alturaRaquete = 90;
+
+function mostraRaquete() {
+    rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete, 5)
+}
+
+function movimentoRaquete() {
+    if (keyIsDown(UP_ARROW)) {
+        yRaquete -= 6
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        yRaquete += 6
+    }
+}
+
+function colisaoRaquete() {
+    if (xBolinha - raio < xRaquete + larguraRaquete && yBolinha - raio < yRaquete + alturaRaquete && yBolinha + raio > yRaquete) {
+        velocidadeXBolinha *= -1;
     }
 }
